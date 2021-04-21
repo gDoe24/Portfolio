874a6aa6f0d0a16c298c4c05bd2e7ada4fc2bc48
gDoe24 <gregkeel11@gmail.com>
2021-04-21 12:09:24 -0400

    feat: Parallax and Section Wipes Implemented Parallax effect and section wipes for homepage

--- frontend/src/components/home/Cover.js
+++ frontend/src/components/home/Cover.js
@@ -5,26 +5,6 @@ import man from "../../../public/man.png";
 
 
 export default function Cover(){
-
-    
-    let timeline = new TimelineMax();
-    let controller = new ScrollMagic.Controller();
-
-    useEffect(() => {
-        timeline
-        .to('.background-rock', 3, {y: -300})
-        .to('.background-man', 3, {y: -200}, '-=3')
-        .to('.background', 3, {y: 50}, '-=3')
-        .to('.about-me', 3, {top: 0}, '-=3');
-        let scene = new ScrollMagic.Scene({
-            triggerElement: "section",
-            duration: "100%",
-            triggerHook: 0,
-        })
-        .setTween(timeline)
-        .setPin(".about-me")
-        .addTo(controller)
-    },[])
     
     return (
         <section className="hero-banner">
@@ -34,18 +14,20 @@ export default function Cover(){
                 <h1 id="welcome">Welcome to My Portfolio</h1>
             </div>
             <span className="overlay"/>
-            <img 
-                className="background"
-                alt="background"
-                src={BG}/>
-            <img 
-                className="background-rock"
-                alt="background rock"
-                src={rock}/>
-            <img 
-                className="background-man"
-                alt="background man"
-                src={man}/>
+            <div className="cover-container">
+                <img 
+                    className="background"
+                    alt="background"
+                    src={BG}/>
+                <img 
+                    className="background-rock"
+                    alt="background rock"
+                    src={rock}/>
+                <img 
+                    className="background-man"
+                    alt="background man"
+                    src={man}/>
+            </div>
         </section>
     )
 }
--- frontend/src/components/layout/Home.js
+++ frontend/src/components/layout/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Cover from '../home/Cover';
 import About from '../home/About';
 import Projects from '../home/Projects';
@@ -6,11 +6,33 @@ import '../../styles/home.scss';
 
 export default function Home(){
 
+    let timeline = new TimelineMax();
+    let controller = new ScrollMagic.Controller();
+    
+
+    useEffect(() => {
+        timeline
+        .to('.background-rock', 1, {y: -75})
+        .to('.background-man', 1, {y: -50}, '-=1')
+        .fromTo('.background', {y: -50}, {y: 0, duration: 1}, '-=1')
+        .fromTo("section.about-me",  1, {y:  "100%"}, {y: "0%", ease: Linear.easeNone}, '-=.5')  // in from bottom
+        .fromTo("section.projects", 1, {x: "130%"}, {x: "0%", ease: Linear.easeNone}); // in from right
+        
+        let scene = new ScrollMagic.Scene({
+            triggerElement: ".container",
+            triggerHook: "onLeave",
+            duration: "300%",
+        })
+        .setTween(timeline)
+        .setPin(".container")
+        .addTo(controller)
+    },[])
+
     return (
-        <main>
+        <div className="container">
             <Cover />
             <About />
             <Projects />
-        </main>
+        </div>
     )
 }
